fb16ad01b6d12f40c92d3e079f68a7f9a153597c
BAIHAQI <mbaihaqisukaini@gmail.com>
2024-08-04 22:55:13 +0800

    change to be able to run

--- src/main/java/com/example/JWT/config/JwtService.java
+++ src/main/java/com/example/JWT/config/JwtService.java
@@ -1,5 +1,10 @@
 package com.example.JWT.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -8,13 +13,9 @@ import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-
+@Service
 public class JwtService {
 
     @Value("${application.security.jwt.secret-key}")
